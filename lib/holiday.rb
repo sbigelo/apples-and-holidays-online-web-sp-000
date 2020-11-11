b9df4b6e76669da98bf4be0a4b01372d5557d940
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-11 17:12:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |key, value|
     puts "#{key.capitalize}:"
     value.each do |key1, value2|
-     puts"  #{key1.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+     puts"  #{key1.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{value1.join(", ")}"
     end
   end
   
