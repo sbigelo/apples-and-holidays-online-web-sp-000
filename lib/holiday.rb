0fe178bb48cb6d8059f30a1f39ff50015c28794c
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-11 16:59:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,7 +51,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |key, value|
-    puts key.capitalize
+    puts key.capitalize.to_s
     value.each do |key1, value2|
     puts key1.capitalize
     end
