86167a67a8572a71740b44dfcd379cf074d46983
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-11 16:51:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -37,9 +37,9 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-holiday_hash[:winter][:new_years] = ["Lights", "Party Hats"]
-holiday_hash[:winter]
-
+holiday_hash.map  do |key, value|
+  
+  end
 end
 
 def all_supplies_in_holidays(holiday_hash)
